e0a0c7cae24f44f17029faf3aeca8ebaff984e99
Gerris <gerrisprakash@gmail.com>
2025-09-22 13:08:50 +0530

    Move base url to .env

--- api/AxiosInstance.js
+++ api/AxiosInstance.js
@@ -1,43 +1,37 @@
 import axios from "axios";
 
-
-const baseUrl = 'http://127.0.0.1:8000/'
+const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
 const AxiosInstance = axios.create({
-    baseURL: baseUrl,
-    timeout:5000,
-    headers:{
-        "Content-Type": "application/json",
-        accept: "application/json"
-    }
-    
-})
+  baseURL: baseUrl,
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+    accept: "application/json",
+  },
+});
 
-AxiosInstance.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('Token')
-        if(token){
-            config.headers.Authorization = `token ${token}`
-        }
-        else{
-            config.headers.Authorization = ''
-        }
-        return config
-    }, 
-)
+AxiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("Token");
+  if (token) {
+    config.headers.Authorization = `token ${token}`;
+  } else {
+    config.headers.Authorization = "";
+  }
+  return config;
+});
 
 AxiosInstance.interceptors.response.use(
-    (response) => {
-        return response
-    },
-    (error) => {
-        if(error.response && error.response.status === 401){
-            localStorage.removeItem('Token')
-            window.location.href = '/login'
-        }
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("Token");
+      window.location.href = "/login";
     }
-)
+    return Promise.reject(error);
+  }
+);
 
-
-
-export default AxiosInstance
+export default AxiosInstance;
